d18f8607f150b1db6008b4d1ac3b3667040071b6
Sagar Prasad Mahto <sagarlovev143@gmail.com>
2026-01-31 00:36:22 +0530

    change again

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 // ================= MIDDLEWARE =================
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
